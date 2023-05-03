d4cd270d750cab9ab4ca12a058d99b1112dd246c
taofiqur <taofiqurrohman11@gmail.com>
2023-05-03 12:00:57 +0700

    login intent

--- app/src/main/java/com/example/intentlogin/MainActivity.kt
+++ app/src/main/java/com/example/intentlogin/MainActivity.kt
@@ -4,17 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val pass = findViewById<EditText>(R.id.password)
+        val email = findViewById<EditText>(R.id.email)
+        val btnLoginActivity = findViewById<Button>(R.id.btnLogin)
 
-        btnLogin.setOnClickListener{
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        btnLoginActivity.setOnClickListener {
+            val strEmail = email.text.toString()
+            val passs = pass.text.toString()
+            if (strEmail.isNotBlank() && passs.isNotBlank()) {
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("email", strEmail)
+                startActivity(intent)
+            }
         }
+
+
     }
 }
+
--- app/src/main/java/com/example/intentlogin/MainActivity2.kt
+++ app/src/main/java/com/example/intentlogin/MainActivity2.kt
@@ -2,10 +2,17 @@ package com.example.intentlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val email = findViewById<TextView>(R.id.tvEmail)
+        val data = intent.getStringExtra("email").toString()
+        email.text= data
+
     }
 }
+
